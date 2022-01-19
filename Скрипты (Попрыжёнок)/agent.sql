788330f835777e330d01c989d04ac585e72cd87e
qwertyx236 <84709316+qwertyx236@users.noreply.github.com>
2022-01-19 09:55:26 +0300

    Add files via upload

---
+++ Скрипты (Попрыжёнок)/agent.sql
@@ -0,0 +1,36 @@
+USE [Poprijenok]
+GO
+
+/****** Object:  Table [dbo].[Agent]    Script Date: 19.01.2022 9:53:53 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Agent](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[Title] [nvarchar](150) NOT NULL,
+	[AgentTypeID] [int] NOT NULL,
+	[Address] [nvarchar](300) NULL,
+	[INN] [varchar](12) NOT NULL,
+	[KPP] [varchar](9) NULL,
+	[DirectorName] [nvarchar](100) NULL,
+	[Phone] [nvarchar](20) NOT NULL,
+	[Email] [nvarchar](255) NULL,
+	[Logo] [nvarchar](100) NULL,
+	[Priority] [int] NOT NULL,
+ CONSTRAINT [PK_Agent] PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[Agent]  WITH CHECK ADD  CONSTRAINT [FK_Agent_AgentType] FOREIGN KEY([AgentTypeID])
+REFERENCES [dbo].[AgentType] ([ID])
+GO
+
+ALTER TABLE [dbo].[Agent] CHECK CONSTRAINT [FK_Agent_AgentType]
+GO
+
---
+++ Скрипты (Попрыжёнок)/product.sql
@@ -0,0 +1,34 @@
+USE [Poprijenok]
+GO
+
+/****** Object:  Table [dbo].[Product]    Script Date: 19.01.2022 9:54:20 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Product](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[Title] [nvarchar](100) NOT NULL,
+	[ProductTypeID] [int] NULL,
+	[ArticleNumber] [nvarchar](10) NOT NULL,
+	[Description] [nvarchar](max) NULL,
+	[Image] [nvarchar](100) NULL,
+	[ProductionPersonCount] [int] NULL,
+	[ProductionWorkshopNumber] [int] NULL,
+	[MinCostForAgent] [decimal](10, 2) NOT NULL,
+ CONSTRAINT [PK_Product] PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[Product]  WITH CHECK ADD  CONSTRAINT [FK_Product_ProductType] FOREIGN KEY([ProductTypeID])
+REFERENCES [dbo].[ProductType] ([ID])
+GO
+
+ALTER TABLE [dbo].[Product] CHECK CONSTRAINT [FK_Product_ProductType]
+GO
+
